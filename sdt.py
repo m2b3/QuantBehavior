107fa561875b1ebc59f687e7776f4901c62c9c01
bsureshkrishna <98675732+bsureshkrishna@users.noreply.github.com>
2026-09-08 09:58:40 -0400

    Declare marimo notebook dependencies

--- sdt.py
+++ sdt.py
@@ -1,3 +1,15 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "marimo>=0.23.9",
+#     "matplotlib",
+#     "numpy",
+#     "psignifit",
+#     "scikit-learn",
+#     "scipy",
+# ]
+# ///
+
 import marimo
 
 __generated_with = "0.23.9"
